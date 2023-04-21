69f84465b04c24524d315193eb19d62bca2c352d
wendell00 <wendellborges2003@gmail.com>
2023-04-21 20:42:18 -0300

    Fazendo a Section Product

--- src/components/sectionProducts/index.tsx
+++ src/components/sectionProducts/index.tsx
@@ -1,18 +1,126 @@
-import { SectionStyles } from './styles'
+import { SectionStyles } from "./styles";
+import { useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export const SectionProducts = () =>{
-    return(
-        <SectionStyles>
-            <div className='productsContainer'>
-                <div className='title-products'>
-                    <h1> <b>FEATURED</b> PRODUCTS </h1>
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-flip";
+
+export const SectionProducts = () => {
+
+  const [bottle50, setBottle50] = useState('./bottle50/mountainberryblast.png')  
+  const [bottleZero, setBottleZero] = useState('./productZero/zsmixedberry.png')  
+
+  function handleClick(url: string, product: string){
+    if(product == '50') setBottle50(url)
+    else if(product == 'zero') setBottleZero(url)
+  }
+
+  return (
+    <SectionStyles>
+      <div className="productsContainer">
+        <div className="title-products">
+          <h1>
+            {" "}
+            <b>FEATURED</b> PRODUCTS{" "}
+          </h1>
+        </div>
+        <div className="section-products-container">
+          <div className="section-1">
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log("slide change")}
+            >
+              <SwiperSlide>
+                <div className="p1">
+                  <img src={bottle50} alt="" />
                 </div>
-                <div className='section-products-container'>
-                    <div className="section-1">
+                <div className="p2 blueBg">
+                  <div className="textContainer">
+                    <div className="containerContent">
+                        <h1>POWERADE 50% MORE ELECTROLYTES</h1>
+                        <p className="description colorWhite">
+                        The ION4 Advanced Electrolyte System helps replenish four
+                        electrolytes lost in sweat. Powerade is formulated with a
+                        6% carbohydrate solution to help provide energy to working
+                        muscles, and vitamins B3, B6 and B12.Powerade
+                        </p>
 
+                        <div className="changeBottleContainer">
+                        <div className="circles-p1">
+                            <button className="blueCircle" onClick={() =>{handleClick('./bottle50/mountainberryblast.png', '50')}}></button>
+                            <button className="orangeCircle" onClick={() =>{handleClick('./bottle50/orange.png', '50')}}></button>
+                            <button className="whiteCircle" onClick={() =>{handleClick('./bottle50/whitecherry.png', '50')}}></button>
+                            <button className="greenCircle" onClick={() =>{handleClick('./bottle50/melon.png', '50')}}></button>
+                            <button className="purpleCircle" onClick={() =>{handleClick('./bottle50/grape.png', '50')}}></button>
+                        </div>
+                        </div>
+
+                        <div>
+                        <button className="btn-Learn">LEAN MORE</button>
+                        </div>
+                        <p className="ingredients">
+                        *Per 12 fl oz: POWERADE • 240mg (Sodium), 80mg
+                        (Potassium); Leading Sports Drink - 160mg (Sodium), 50mg
+                        (Potassium), © 2023 The Coca-Cola Company.
+                        </p>
                     </div>
+                  </div>
                 </div>
-            </div>
-        </SectionStyles>
-    )
-}
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="p1">
+                  <img src={bottleZero} alt="" />
+                </div>
+                <div className="p2 whiteBg">
+                  <div className="textContainer">
+                    <div className="containerContent">
+                        <h1>POWERADE ZERO</h1>
+                        <p className="description colorBlack">
+                        POWERADE Zero is a zero-calorie sports drink that provides advanced hydration for players who are serious about their game. A 6% carbohydrate solution provides energy to working muscles, plus vitamins B3, B6, and B12. It's the only nationally available sports drink with the ION4 advanced electrolyte system that replenishes four electrolytes lost in sweat.
+                        </p>
+
+                        <div className="changeBottleContainer">
+                        <div className="circles-p1">
+                            <button className="blueCircle" onClick={() =>{handleClick('./productZero/zsmixedberry.png', 'zero')}}></button>
+                            <button className="orangeCircle" onClick={() =>{handleClick('./productZero/zsorange.png', 'zero')}}></button>
+                            <button className="whiteCircle" onClick={() =>{handleClick('./productZero/zswhitecherry.png', 'zero')}}></button>
+                            <button className="redCircle" onClick={() =>{handleClick('./productZero/zsfruitpunch.png', 'zero')}}></button>
+                            <button className="purpleCircle" onClick={() =>{handleClick('./productZero/zsgrape.png', 'zero')}}></button>
+                        </div>
+                        </div>
+
+                        <div>
+                        <button className="btn-Learn">LEAN MORE</button>
+                        </div>
+                        <p className="ingredients">
+                        water, less than 1% of: citric acid, salt and mono-potassium phosphate and magnesium chloride and calcium chloride (electrolyte sources), natural flavors, sucralose, acesulfame potassium, vitamin b3 (niacinamide), vitamin b6 (pyridoxine hydrochloride), vitamin b12 (cyanocobalamin), blue 1, ascorbic acid (to protect taste), calcium disodium edta (to protect color).
+                        </p>
+                    </div>
+                    
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="p1">
+                  <img src="./productBars.png" alt="" />
+                </div>
+                <div className="p2 blackBg"></div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="p1"></div>
+                <div className="p2 greenBg"></div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </SectionStyles>
+  );
+};
--- src/components/sectionProducts/styles.ts
+++ src/components/sectionProducts/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const SectionStyles = styled.section`
     .productsContainer{
         width: 100vw;
@@ -23,14 +22,196 @@ export const SectionStyles = styled.section`
             display: flex;
             justify-content: center;
             align-items: center;
+            
 
             .section-1{
                 width: 86%;
                 height: 100%;
-                background-color: #c2e7f5;
                 border-radius: 60px;
+                
+
+                .swiper{
+                    height: 100%;   
+                    border-radius: 60px;
+                    border: 1px solid black;
+                    background-color: rgba(255,255,255, .6);
+                    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+
+                    
+                    .swiper-wrapper{
+                        height: 100%;
+    
+                        .swiper-slide{
+                            height: 100%;
+                            display: flex;
+
+
+                            .p1{
+                                width: 35%;
+                                height: 100%;
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+
+                                img{
+                                    width: auto;
+                                    height: 90%;
+                                }
+                            }
+
+                            .p2{
+                                width: 65%;
+                                height: 100%;
+                                clip-path: polygon(21% 0, 100% 0, 100% 100%, 0% 100%);
+                                display: flex;
+                                align-items: center;
+                                justify-content: end;
+                                
+
+                                .textContainer{
+                                    width: 80%;
+                                    height: auto;
+                                    display: flex;
+                                    justify-content: center;
+                                    align-items: center;
+                                    
+                                    
+                                    .containerContent{
+                                        width: 100%;
+                                        height: 100%;
+
+                                        
+                                        h1{
+                                            font-size 3em;
+                                            color: black;
+                                            margin-right: 30px;
+                                            text-align: center;
+                                            
+                                        }
+    
+                                        .description{
+                                            font-weight: bold;
+                                            text-align: center;
+                                            font-size: 1.1em;
+                                            font-family: 'Arial';
+                                            width: 95%;
+                                        }
+    
+                                        .changeBottleContainer{
+                                            width: 100%;
+                                            height: 150px;
+                                            display: flex;
+                                            justify-content: center;
+                                    
+                                            .circles-p1{
+                                                width: 100%;
+                                                display: flex;
+                                                align-items: center;
+                                                justify-content: space-around;
+                                    
+                                                button{
+                                                    width: 64px;
+                                                    height: 64px;
+                                                    border-radius: 100%;
+                                                    border: 2px solid black;
+                                                    cursor: pointer;
+                                                }
+                                            }
+                                        }
+                                
+                                        div{
+                                            width: 100%;
+                                            height: auto;
+                                            display: flex;
+                                            justify-content: center;
+                                            margin-bottom: 15px;
+    
+                                            .btn-Learn{
+                                                padding: 10px 20px;;
+                                                background-color: black;
+                                                color: white;
+                                                border: 3px solid black;
+                                                border-radius: 5px;
+                                                font-family: "Arial", "sans-serif";
+                                                font-weight: bold;
+                                                font-size: 18px;
+                                                cursor: pointer;
+        
+                                                :hover{
+                                                    background-color: transparent;
+                                                    color: black;
+                                                }
+                                            }
+                                        }
+    
+                                        .ingredients{
+                                            color: black;
+                                            font-weight: bold;
+                                            text-align: center;
+                                            font-size: 0.8em;
+                                            font-family: "Arial", "sans-serif";
+                                            width: 95%;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
             }
         }
     }
 
+    .blackBg{
+        background-color: #000;
+    }
+
+    
+    .blueBg{
+        background-color: #04adee;
+    }
+
+    
+    .whiteBg{
+        background-color: #CEC2D0;
+    }
+
+    
+    .greenBg{
+        background-color: green;
+    }
+
+    .whiteCircle{
+        background-color: #CEC2D0;
+    }
+
+    .blueCircle{
+        background-color: #04adee;
+    }
+
+    .greenCircle{
+        background-color: #8CDA00;
+    }
+
+    .orangeCircle{
+        background-color: #FF6E00;
+    }
+
+    .purpleCircle{
+        background-color: #8149B9;
+    }
+
+    .redCircle{
+        background-color: #E30100;
+    }
+
+    .colorBlack{
+        color: black;
+    }
+
+    .colorWhite{
+        color: white;
+    }
 `;
